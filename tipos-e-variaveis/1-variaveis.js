8a55dab02472183e0198221fd087f28bee4323f4
Jane Fernanda Martins <33935791+jfermartins@users.noreply.github.com>
2022-05-23 14:18:22 -0300

    Variáveis

---
+++ tipos-e-variaveis/1-variaveis.js
@@ -0,0 +1,7 @@
+var nameVar = "Jane";
+let nameLet = "Jane";
+const nameConst = "Jane";
+
+console.log(`nameVar: ${nameVar}`);
+console.log(`nameLet: ${nameLet}`);
+console.log(`nameConst: ${nameConst}`);
